a129e46b30bc631a749590ccf692f346c0e53887
joseph lebiane <joseph.lebiane@outlook.com>
2023-05-15 13:53:41 -0700

    fix typo

--- src/redux/services/billboard.js
+++ src/redux/services/billboard.js
@@ -5,10 +5,7 @@ export const billboardApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://billboard-api5.p.rapidapi.com/api/charts",
     prepareHeaders: (headers) => {
-      headers.set(
-        "X-RapidAPI-Key",
-        "a3531ddad3msh35dc2f5862028a5p14ef04jsn95155fbc9398"
-      );
+      headers.set("X-RapidAPI-Key", import.meta.env.VITE_RAPID_API_KEY);
       headers.set("X-RapidAPI-Host", "billboard-api5.p.rapidapi.com");
       return headers;
     },
--- src/redux/services/shopify.js
+++ src/redux/services/shopify.js
@@ -5,10 +5,7 @@ export const shopifyApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://spotify23.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set(
-        "X-RapidAPI-Key",
-        "a3531ddad3msh35dc2f5862028a5p14ef04jsn95155fbc9398"
-      );
+      headers.set("X-RapidAPI-Key", import.meta.env.VITE_RAPID_API_KEY);
       headers.set("X-RapidAPI-Host", "spotify23.p.rapidapi.com");
       return headers;
     },
